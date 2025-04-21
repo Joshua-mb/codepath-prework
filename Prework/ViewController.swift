a548b64513e28f7e936352a497aaf0289b98b431
Joshua-mb <54364349+Joshua-mb@users.noreply.github.com>
2025-04-20 21:29:17 -0500

    Added prework files to repo

---
+++ Prework/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Joshua Mburu on 4/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let newColor = changeColor()
+        self.view.backgroundColor = newColor
+    }
+    func changeColor() -> UIColor{
+
+           let red = CGFloat.random(in: 0...1)
+           let green = CGFloat.random(in: 0...1)
+           let blue = CGFloat.random(in: 0...1)
+
+           return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+       }
+    
+}
+
---
+++ PreworkTests/PreworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  PreworkTests.swift
+//  PreworkTests
+//
+//  Created by Joshua Mburu on 4/20/25.
+//
+
+import Testing
+@testable import Prework
+
+struct PreworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
